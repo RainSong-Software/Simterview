6a7e4e8ed8ee62968cf59b02f78a818bcd2863dc
Jingxi Deng <samueldeng78@gmail.com>
2025-06-05 01:19:06 -0700

    fixing home page button hydration issues

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "sonner";
+import StyledComponentsRegistry from "@/lib/styled-components-registry";
 
 const inter = Inter({
   variable: "--font-inter",
@@ -81,7 +82,7 @@ export default function RootLayout({
       <body
         className={`${inter.className} antialiased pattern`}
       >
-        {children}
+        <StyledComponentsRegistry>{children}</StyledComponentsRegistry>
         <Toaster />
       </body>
     </html>
